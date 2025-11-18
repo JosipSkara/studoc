0749ee49b1002a6b0b1218aa0b194369df4d50b5
JosipSkara <josipskara31@gmail.com>
2025-11-18 22:28:30 +0100

    safety commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     const [message, setMessage] = useState("Lade Backend...");
 
     useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL}/api/hello`)
+        fetch(`/api/hello`)
             .then((res) => res.json())
             .then((data) => setMessage(data.message))
             .catch((err) => {
